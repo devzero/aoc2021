861919ab0ec9f04515e3715f37dadbcad2cfc05c
Chris Connelly <github@logon.pw>
2021-12-04 18:39:19 -0500

    formatting

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use itertools::Itertools;
+use std::fs;
 
 type Data = Vec<u32>;
 
@@ -7,35 +7,31 @@ fn parse(fname: &str) -> Data {
     String::from_utf8(fs::read(fname).expect("missing file"))
         .unwrap()
         .lines()
-        .map(|x| 
-             x
-                .parse()
-                .unwrap()
-            )
+        .map(|x| x.parse().unwrap())
         .collect()
 }
 
 #[test]
-fn test_part1(){
+fn test_part1() {
     assert_eq!(part1(parse("test0")), 7)
 }
 
 fn part1(data: Data) -> usize {
     data.into_iter()
         .tuple_windows()
-        .filter(|(prev,next)| next>prev)
+        .filter(|(prev, next)| next > prev)
         .count()
 }
 
 #[test]
-fn test_part2(){
+fn test_part2() {
     assert_eq!(part2(parse("test0")), 5)
 }
 
 fn part2(data: Data) -> usize {
-    let windowed:Vec<u32> = data
+    let windowed: Vec<u32> = data
         .into_iter()
-        .tuple_windows::<(_,_,_)>()
+        .tuple_windows::<(_, _, _)>()
         .map(|(x, y, z)| x + y + z)
         .collect();
     part1(windowed)
