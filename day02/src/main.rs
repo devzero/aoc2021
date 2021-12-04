bad7f9f19f22457925f56eb723ec15150260ede1
Chris Connelly <github@logon.pw>
2021-12-04 18:38:47 -0500

    formatting

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,18 +1,18 @@
 use std::fs;
 
 enum Instr {
-  Up(usize),
-  Down(usize),
-  Forward(usize)
+    Up(usize),
+    Down(usize),
+    Forward(usize),
 }
 
-fn to_instr(line:&str) -> Instr {
-    let tokens:Vec<&str> = line.split_ascii_whitespace().collect();
+fn to_instr(line: &str) -> Instr {
+    let tokens: Vec<&str> = line.split_ascii_whitespace().collect();
     match (tokens[0], tokens[1].parse::<usize>().unwrap()) {
         ("down", num) => Instr::Down(num),
         ("up", num) => Instr::Up(num),
         ("forward", num) => Instr::Forward(num),
-        _ => panic!()
+        _ => panic!(),
     }
 }
 
@@ -25,35 +25,37 @@ fn parse(fname: &str) -> Vec<Instr> {
 }
 
 #[test]
-fn test_part1(){
+fn test_part1() {
     assert_eq!(part1(parse("test0")), 150)
 }
 
 fn part1(instrs: Vec<Instr>) -> usize {
-    let (h,d) = instrs
+    let (h, d) = instrs
         .iter()
-        .fold((0usize,0usize), |(horiz,depth), instr| match instr {
-            Instr::Up(x) => (horiz,depth-x),
-            Instr::Down(x) => (horiz, depth+x),
-            Instr::Forward(x) => (horiz+x, depth),
+        .fold((0usize, 0usize), |(horiz, depth), instr| match instr {
+            Instr::Up(x) => (horiz, depth - x),
+            Instr::Down(x) => (horiz, depth + x),
+            Instr::Forward(x) => (horiz + x, depth),
         });
-    h*d
+    h * d
 }
 
 #[test]
-fn test_part2(){
+fn test_part2() {
     assert_eq!(part2(parse("test0")), 900)
 }
 
 fn part2(instrs: Vec<Instr>) -> usize {
-    let (h,d, _) = instrs
-        .iter()
-        .fold((0usize,0usize,0usize), |(horiz,depth, aim), instr| match instr {
-            Instr::Up(x) => (horiz, depth, aim-x),
-            Instr::Down(x) => (horiz, depth, aim+x),
-            Instr::Forward(x) => (horiz+x, depth+(aim*x), aim),
-        });
-    h*d
+    let (h, d, _) =
+        instrs.iter().fold(
+            (0usize, 0usize, 0usize),
+            |(horiz, depth, aim), instr| match instr {
+                Instr::Up(x) => (horiz, depth, aim - x),
+                Instr::Down(x) => (horiz, depth, aim + x),
+                Instr::Forward(x) => (horiz + x, depth + (aim * x), aim),
+            },
+        );
+    h * d
 }
 
 fn main() {
